9715d6376030d4cb11f74b736d9e605a1581992e
haloapping <alfiyanto.kondolele@gmail.com>
2024-10-22 14:07:04 +0700

    add some stuff

---
+++ selector/home_page.py
@@ -0,0 +1,12 @@
+# link
+LOGIN_SIGNUP_LINK = "#header > div > div > div > div.col-sm-8 > div > ul > li:nth-child(4) > a"
+
+# login
+login_email_address_input_text = "#form > div > div > div.col-sm-4.col-sm-offset-1 > div > form > input[type=email]:nth-child(2)"
+password_input_text = "#form > div > div > div.col-sm-4.col-sm-offset-1 > div > form > input[type=password]:nth-child(3)"
+login_button = "#form > div > div > div.col-sm-4.col-sm-offset-1 > div > form > button"
+
+# signup
+NAME_INPUT_TEXT = "#form > div > div > div:nth-child(3) > div > form > input[type=text]:nth-child(2)"
+SIGNUP_EMAIL_ADDRESS_INPUT_TEXT = "#form > div > div > div:nth-child(3) > div > form > input[type=email]:nth-child(3)"
+SIGNUP_BUTTON = "#form > div > div > div:nth-child(3) > div > form > button"
---
+++ selector/signup_page.py
@@ -0,0 +1,24 @@
+# account information
+title_input_text = ""
+PASSWORD_INPUT_TEXT = "#password"
+DATE_OF_BIRTH_DAY_DROPDOWN = "#days"
+DATE_OF_BIRTH_MONTH_DROPDOWN = "#months"
+DATE_OF_BIRTH_YEAR_DROPDOWN = "#years"
+
+# address information
+FIRSTNAME_INPUT_TEXT = "#first_name"
+LASTNAME_INPUT_TEXT = "#last_name"
+COMPANY_INPUT_TEXT = "#company"
+ADDRESS1_INPUT_TEXT = "#address1"
+ADDRESS2_INPUT_TEXT = "#address2"
+COUNTRY_DROPDOWN = "#country"
+STATE_INPUT_TEXT = "#state"
+CITY_INPUT_TEXT = "#city"
+ZIPCODE_INPUT_TEXT = "#zipcode"
+MOBILE_NUMBER_INPUT_TEXT = "#mobile_number"
+
+CREATE_ACCOUNT_BUTTON = "#form > div > div > div > div.login-form > form > button"
+
+CONTINUE_BUTTON = "#form > div > div > div > div > a"
+
+ENTER_ACCOUNT_INFO_TEXT = "#form > div > div > div > div.login-form > h2 > b"
